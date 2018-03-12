4204fc00d0b9be5b6362e63c19eb8362b3e0c7ae
John Mayer <dev.mayer.john@dev.bwater.com>
2018-03-12 17:58:44 -0400

    fix drive id

--- prep_drives.sh
+++ prep_drives.sh
@@ -2,5 +2,5 @@
 sgdisk -og $1
 sgdisk -n 1:0:+100K -c 1:"BIOS Boot Partition" -t 1:ef02 $1
 sgdisk -n 2:0:+200M -c 2:"EFI System Partition" -t 2:ef00 $1
-sgdisk -n 3:0:0 -c 3:"Linux LVM" -t 4:8e00 $1
+sgdisk -n 3:0:0 -c 3:"Linux LVM" -t 3:8e00 $1
 sgdisk -p $1
